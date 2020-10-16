31de83bb3c9faef1ce4a8e033d02d8ad43bc69e1
Jon Yoder <jsyoder@mailfence.com>
2020-10-16 06:42:08 -0400

    Add missing curly brace

--- gostringlist.go
+++ gostringlist.go
@@ -23,6 +23,7 @@ func New() *StringList {
 	var newList StringList
 	newList.Items = make([]string, 0)
 	return &newList
+}
 
 // Append appends a string to the list
 func (list *StringList) Append(str string) {
